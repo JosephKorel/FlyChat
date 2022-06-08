5e94c565f20301257bd314a8f3f6db19522e7782
José Carlos Rolim Etchepare <jcjosecarlos03@gmail.com>
2022-06-08 13:50:10 -0300

    hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import Home from "./Components/Home";
 import { AppContextProvider, AppContext } from "./Context/AuthContext";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation,
-} from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Profile from "./Components/profile";
 import ChatPage from "./Components/chat";
